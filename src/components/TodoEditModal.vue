42a7c8bce7fd4344b3b021820eeaae2be7ddbf04
Adam Bailey <adamjbailey7@gmail.com>
2019-09-19 16:47:29 -0700

    Change values with a modal

---
+++ src/components/TodoEditModal.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="modal-card" style="width: auto">
+    <header class="modal-card-head">
+      <p class="modal-card-title">Edit Todo</p>
+    </header>
+    <section class="modal-card-body">
+      <b-field label="Title">
+        <b-input type="text" v-model="title" placeholder="Your todo title">
+        </b-input>
+      </b-field>
+
+      <b-field label="Priority">
+        <b-select placeholder="Select a priority" v-model="priority">
+          <option
+            v-for="option in priorities"
+            :value="option.name"
+            :key="option.id"
+          >
+            {{ option.name }}
+          </option>
+        </b-select>
+      </b-field>
+    </section>
+    <footer class="modal-card-foot">
+      <button class="button" type="button" @click="$parent.close()">
+        Close
+      </button>
+      <button class="button is-primary" @click="editTodo">Save</button>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoEditModal",
+  props: {
+    todo: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      title: "",
+      priority: "",
+      priorities: [
+        { id: 1, name: "life changing" },
+        { id: 2, name: "important" },
+        { id: 3, name: "meh" }
+      ]
+    };
+  },
+  mounted() {
+    this.title = this.todo.todo;
+    this.priority = this.todo.priority;
+  },
+  methods: {
+    editTodo() {
+      const payload = {
+        id: this.todo.id,
+        todo: this.title,
+        priority: this.priority
+      };
+      this.$emit("edit-todo", payload);
+    }
+  }
+};
+</script>
+
+<style scoped></style>
--- src/components/TodoTable.vue
+++ src/components/TodoTable.vue
@@ -1,32 +1,51 @@
 <template>
-  <section class="section">
-    <div class="container">
-      <b-table :data="data">
-        <template slot-scope="data">
-          <b-table-column field="id" label="ID" width="40" sortable numeric>
-            {{ data.row.id }}
-          </b-table-column>
+  <div>
+    <section class="section">
+      <div class="container">
+        <b-table :data="todos" default-sort="priority">
+          <template slot-scope="todos">
+            <b-table-column field="id" label="ID" width="40" sortable numeric>
+              {{ todos.row.id }}
+            </b-table-column>
 
-          <b-table-column field="todo" label="Todo" sortable>
-            {{ data.row.todo }}
-          </b-table-column>
+            <b-table-column field="todo" label="Todo" sortable>
+              {{ todos.row.todo }}
+            </b-table-column>
 
-          <b-table-column field="priority" label="Priority" sortable>
-            {{ data.row.priority }}
-          </b-table-column>
-        </template>
-      </b-table>
-    </div>
-  </section>
+            <b-table-column field="priority" label="Priority" sortable>
+              {{ todos.row.priority }}
+            </b-table-column>
+
+            <b-table-column label="Edit">
+              <b-button
+                type="is-text"
+                icon-left="settings-outline"
+                @click="openEditModal(todos.row)"
+              ></b-button>
+            </b-table-column>
+          </template>
+        </b-table>
+      </div>
+    </section>
+
+    <b-modal :active.sync="isEditModalActive" has-modal-card>
+      <todo-edit-modal
+        :todo="selectedTodo"
+        @edit-todo="onEditTodo"
+      ></todo-edit-modal>
+    </b-modal>
+  </div>
 </template>
 
 <script>
+import TodoEditModal from "@/components/TodoEditModal";
+
 export default {
   name: "TodoTable",
-
+  components: { TodoEditModal },
   data() {
     return {
-      data: [
+      todos: [
         {
           id: 1,
           todo: "Go on a run",
@@ -52,8 +71,23 @@ export default {
           todo: "Smell the roses",
           priority: "meh"
         }
-      ]
+      ],
+      isEditModalActive: false,
+      selectedTodo: {}
     };
+  },
+  methods: {
+    openEditModal(todo) {
+      this.selectedTodo = todo;
+      this.isEditModalActive = true;
+    },
+    async onEditTodo(item) {
+      const todo = await this.todos.find(todo => todo.id === item.id);
+      console.log(todo);
+      todo.todo = item.todo;
+      todo.priority = item.priority;
+      this.isEditModalActive = false;
+    }
   }
 };
 </script>
